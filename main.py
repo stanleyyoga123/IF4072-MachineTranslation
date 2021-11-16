7c03d9fc2f08381a6d841338d425770f728cbc93
stanleyyoga123 <yoga360227@gmail.com>
2021-11-16 07:50:04 +0700

    add argparse

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-if __name__ == '__main__':
-    use = 'attention'
+import argparse
 
-    if use == 't5':
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-a", "--attention", action="store_true", required=False, help="Attention Model"
+    )
+    parser.add_argument(
+        "-t5", "--t5", action="store_true", required=False, help="T5 Model"
+    )
+    args = parser.parse_args()
+
+    if args.t5:
         from src.t5.train import train
         from src.t5.simulate import simulate
 
         # Do Something
-        simulate()
+        train()
 
-    elif use == 'attention':
+    elif args.attention:
         from src.attention.train import train
         from src.attention.translate import translate
         from src.attention.evaluate import evaluate
 
         # Do Something
-        translate('2021-11-08 21.56.14')
+        train()
